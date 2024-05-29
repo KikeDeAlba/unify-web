bcab7198c05887b0d48827eb0cef56446235d036
KikeDeAlba <likingvirus1@gmail.com>
2024-05-28 17:55:57 -0700

    add new callback

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,5 +1,5 @@
 export const AUTH_CALLBACKS = {
     twitch: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3000/api/auth/twitch'
-        : '/api/auth/twitch'
+        : 'https://unify-bot.vercel.app/api/auth/twitch'
 }
