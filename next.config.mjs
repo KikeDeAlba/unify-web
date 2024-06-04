16ca553b835a4d2f4929606a62844dc956f6046b
KikeDeAlba <likingvirus1@gmail.com>
2024-06-03 22:30:52 -0700

    fix access to env variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	env: {
+		NEXT_PUBLIC_TWICTH_CLIENT_ID: "dj8xn9c27boi72qidihmyf650mdavj",
+	},
+};
 
 export default nextConfig;
